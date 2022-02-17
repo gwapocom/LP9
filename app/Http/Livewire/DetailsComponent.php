7e0f947dd5425436cf755cbb92b90e30fa59787f
IAMGOD <sccccccarlet@gmail.com>
2022-02-17 00:43:03 -0800

    bear with me

---
+++ app/Http/Livewire/DetailsComponent.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Product;
+use Livewire\Component;
+use \Cviebrock\EloquentSluggable\Services\SlugService;
+class DetailsComponent extends Component
+{
+     public $slug;
+    
+     public function mount($slug)
+     {
+
+         $this->slug = $slug;
+     }
+
+
+     public function render()
+     {
+         $product = Product::where('slug',$this->slug)->first();
+        return view('livewire.details-component',['product'=>$product])->layout('layouts.base');
+     }
+
+    
+}
